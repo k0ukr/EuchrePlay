39a71fec661399a0a1bf12b432c4a74166de7d0f
Mark Dolan <mdolan@cottinghambutler.com>
2022-03-30 22:36:06 -0500

    Missing files

---
+++ OnlineEuchre/OnlineEuchre/Classes/PlayerManager.cs
@@ -0,0 +1,182 @@
+﻿using OnlineEuchre.Extras;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Timers;
+using System.Windows.Forms;
+using static OnlineEuchre.Extras.Randomizer;
+
+namespace OnlineEuchre.Classes
+{
+    public class PlayerManager
+    {
+        frmAsk AskForm = new frmAsk();
+        List<int> lstDeck = null;
+        System.Timers.Timer EuchreTimer = null;
+        System.Timers.Timer CallTimer = null;
+        private frmMain _frmMain { get; set; }
+        private static bool GoAheadEuchre = false;
+        private static bool GoAheadCall = false;
+        private int DealerID { get; set; }
+        private int PlayerID { get; set; }
+        private int TrumpID { get; set; }
+
+        Dictionary<int, Player> dictPlayer = new Dictionary<int, Player>();
+        public PlayerManager (frmMain formMain, Player p1, Player p2, Player p3, Player p4)
+        {
+            _frmMain = formMain;
+            EuchreTimer = new System.Timers.Timer(1000);
+            EuchreTimer.Elapsed += new ElapsedEventHandler(OnEuchreTimedEvent);
+
+            CallTimer = new System.Timers.Timer(3000);
+            CallTimer.Elapsed += new ElapsedEventHandler(OnCallTimedEvent);
+
+            DealerID = 4;
+            PlayerID = 1;
+            Constants.gAppPath = Path.GetDirectoryName(Application.ExecutablePath);
+            LoadCards.LoadImages(Constants.gAppPath);
+            dictPlayer.Add(1, p1);
+            dictPlayer.Add(2, p2);
+            dictPlayer.Add(3, p3);
+            dictPlayer.Add(4, p4);
+        }
+
+        private static void OnEuchreTimedEvent(object source, ElapsedEventArgs e)
+        {
+            ((System.Timers.Timer)source).Stop();
+            GoAheadEuchre = true;
+        }
+
+        private static void OnCallTimedEvent(object source, ElapsedEventArgs e)
+        {
+            ((System.Timers.Timer)source).Stop();
+            GoAheadCall = true;
+        }
+
+        public void Deal()
+        {
+            Shuffle();
+            AdvanceDealer();
+            if (AskForm == null)
+            {
+                AskForm = new frmAsk();
+            }
+            AskForm.Top = _frmMain.GetAnchorLocation().Y;
+            AskForm.Left = _frmMain.GetAnchorLocation().X;
+            if (AskForm.ShowDialog() == DialogResult.OK)
+            {
+                _frmMain.SetDiscardMode(true);
+                _frmMain.SetlblDiscardVisibility(true);
+            }
+            int TurnupCard = lstDeck[20];
+        }
+
+        public void TrumpClick()
+        {
+            Player prevPlayer = null;
+            foreach (KeyValuePair<int, Player> keyVal in dictPlayer)
+            {
+                prevPlayer?.ClearTrump();
+                keyVal.Value.SetTrump((Constants.Suit)(keyVal.Key - 1));
+                StartEuchreTimer();
+                while (!GoAheadEuchre) { Application.DoEvents(); };
+                prevPlayer = keyVal.Value;
+            }
+            prevPlayer?.ClearTrump();
+        }
+
+        public void CallClick()
+        {
+            Player prevPlayer = null;
+            foreach (KeyValuePair<int, Player> keyVal in dictPlayer)
+            {
+                prevPlayer?.ClearArrow();
+                keyVal.Value.SetArrow();
+                StartCallTimer();
+                while (!GoAheadCall) { Application.DoEvents(); };
+                prevPlayer = keyVal.Value;
+            }
+            prevPlayer?.ClearArrow();
+        }
+
+        private void HideTurnup()
+        {
+            _frmMain.SetTurnUp(false, null);
+        }
+
+        private void ShowTurnup(int index)
+        {
+            _frmMain.SetTurnUp(true, CommonMod.RotateBitmap(LoadCards.GetByElement(index - 1).cImage, Constants.Vertical));
+        }
+
+        public void Shuffle()
+        {
+            HideTurnup();
+            dictPlayer[1].LoadBack();
+            dictPlayer[2].LoadBack();
+            dictPlayer[3].LoadBack();
+            dictPlayer[4].LoadBack();
+        }
+
+        public void AdvanceDealer()
+        {
+            int index = 1;
+            lstDeck = RandomMod.GetRandomList(24);
+            dictPlayer[DealerID].UpdateDealer(DealerID, false);
+            dictPlayer[DealerID].UpdateCall(DealerID, false);
+            DealerID = CommonMod.lstNextWhatever[DealerID - 1];
+            dictPlayer[DealerID].UpdateDealer(DealerID, true);
+            dictPlayer[DealerID].UpdateCall(DealerID, true);
+            int playerId = DealerID;
+            for (int i = 0; i < 4; i++)
+            {
+                StartEuchreTimer();
+                while (!GoAheadEuchre) { Application.DoEvents(); };
+
+                playerId = CommonMod.lstNextWhatever[playerId - 1];
+                dictPlayer[playerId].ClearHand();
+                for (int cardex = 1; cardex < 6; cardex++)
+                {
+                    dictPlayer[playerId].LoadHand(cardex, lstDeck[index++]);
+                }
+            }
+            ShowTurnup(lstDeck[index++]);
+        }
+
+        public void StartEuchreTimer()
+        {
+            EuchreTimer.Start();
+            GoAheadEuchre = false;
+        }
+
+        public void StopEuchreTimer()
+        {
+            EuchreTimer.Stop();
+            GoAheadEuchre = true;
+        }
+
+        public void StartCallTimer()
+        {
+            CallTimer.Start();
+            GoAheadCall = false;
+        }
+
+        public void StopCallTimer()
+        {
+            CallTimer.Stop();
+            GoAheadCall = true;
+        }
+
+        public void Common_PictureBoxClick(object sender, ref bool discardMode)
+        {
+            if (discardMode)
+            {
+                dictPlayer[DealerID].Pickup((PictureBox)sender, lstDeck[Constants.TURNUP_INDEX]);
+                Application.DoEvents();
+                discardMode = false;
+                _frmMain.SetlblDiscardVisibility(false);
+            }
+
+        }
+    }
+}
---
+++ OnlineEuchre/OnlineEuchre/frmAsk.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace OnlineEuchre
+{
+    partial class frmAsk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPass = new System.Windows.Forms.Button();
+            this.btnCall = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPass
+            // 
+            this.btnPass.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnPass.Location = new System.Drawing.Point(12, 12);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.Size = new System.Drawing.Size(75, 28);
+            this.btnPass.TabIndex = 0;
+            this.btnPass.Text = "Pass";
+            this.btnPass.UseVisualStyleBackColor = true;
+            // 
+            // btnCall
+            // 
+            this.btnCall.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCall.Location = new System.Drawing.Point(93, 12);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(75, 28);
+            this.btnCall.TabIndex = 1;
+            this.btnCall.Text = "Call";
+            this.btnCall.UseVisualStyleBackColor = true;
+            // 
+            // frmAsk
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(183, 54);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnCall);
+            this.Controls.Add(this.btnPass);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAsk";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "frmAsk";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPass;
+        private System.Windows.Forms.Button btnCall;
+    }
+}
